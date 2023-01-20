648bb788b14d7a965831c0a5357471b57b72d18f
HuaJiKing <3550514081@qq.com>
2023-01-20 21:01:30 +0800

    + Client-Ava

---
+++ Client-Ava/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Client_Ava
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
