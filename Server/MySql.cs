9e0eda4df3c4a2e750206be06a030dd01f612b3b
HuaJiKing402 <35505140841@qq.com>
2023-02-09 23:02:31 +0800

    #修改

--- Server/MySql.cs
+++ Server/MySql.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
-using MySqlConnector;
+using MySql.Data.MySqlClient;
 
 namespace Server
 {
