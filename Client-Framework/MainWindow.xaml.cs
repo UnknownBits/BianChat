732585b71bac63c031b6bf3785d46f330ad4c01c
HuaJiKing402 <35505140841@qq.com>
2023-02-10 13:08:02 +0800

    #修复

--- Client-Framework/MainWindow.xaml.cs
+++ Client-Framework/MainWindow.xaml.cs
@@ -13,10 +13,10 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Client_WPF
+namespace Client_Framework
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// MainWindow.xaml 的交互逻辑
     /// </summary>
     public partial class MainWindow : Window
     {
