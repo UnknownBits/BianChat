fc9f0703ea260bb85632b3c803bcd44a37f96b60
WinExp <1450422389@qq.com>
2023-02-18 21:52:14 +0800

    * 更改为 ModernWpf

--- Client-WPF/MainWindow.xaml.cs
+++ Client-WPF/MainWindow.xaml.cs
@@ -19,7 +19,7 @@ namespace Client_WPF
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : UiWindow
+    public partial class MainWindow : Window
     {
         public MainWindow()
         {
