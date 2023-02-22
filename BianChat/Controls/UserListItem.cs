c04facdd6242ea8989bdca9bf86320a7f40913f6
WinExp <1450422389@qq.com>
2023-02-22 20:07:39 +0800

    * 修改 UI

---
+++ BianChat/Controls/UserListItem.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BianChat.Controls
+{
+    /// <summary>
+    /// 按照步骤 1a 或 1b 操作，然后执行步骤 2 以在 XAML 文件中使用此自定义控件。
+    ///
+    /// 步骤 1a) 在当前项目中存在的 XAML 文件中使用该自定义控件。
+    /// 将此 XmlNamespace 特性添加到要使用该特性的标记文件的根
+    /// 元素中:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:BianChat.Controls"
+    ///
+    ///
+    /// 步骤 1b) 在其他项目中存在的 XAML 文件中使用该自定义控件。
+    /// 将此 XmlNamespace 特性添加到要使用该特性的标记文件的根
+    /// 元素中:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:BianChat.Controls;assembly=BianChat.Controls"
+    ///
+    /// 您还需要添加一个从 XAML 文件所在的项目到此项目的项目引用，
+    /// 并重新生成以避免编译错误:
+    ///
+    ///     在解决方案资源管理器中右击目标项目，然后依次单击
+    ///     “添加引用”->“项目”->[浏览查找并选择此项目]
+    ///
+    ///
+    /// 步骤 2)
+    /// 继续操作并在 XAML 文件中使用控件。
+    ///
+    ///     <MyNamespace:UserListItem/>
+    ///
+    /// </summary>
+    public class UserListItem : Control
+    {
+        static UserListItem()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(UserListItem), new FrameworkPropertyMetadata(typeof(UserListItem)));
+        }
+
+        public static readonly DependencyProperty ImageSourceProperty
+            = DependencyProperty.Register("ImageSource", typeof(ImageSource), typeof(UserListItem));
+
+        public ImageSource ImageSource
+        {
+            get { return (ImageSource)GetValue(ImageSourceProperty); }
+            set { SetValue(ImageSourceProperty, value); }
+        }
+
+        public static readonly DependencyProperty UsernameProperty
+            = DependencyProperty.Register("Username", typeof(string), typeof(UserListItem));
+
+        public string Username
+        {
+            get { return (string)GetValue(UsernameProperty); }
+            set { SetValue(UsernameProperty, value); }
+        }
+
+        public static readonly DependencyProperty LastMessageProperty
+            = DependencyProperty.Register("LastMessage", typeof(string), typeof(UserListItem));
+
+        public string LastMessage
+        {
+            get { return (string)GetValue(LastMessageProperty); }
+            set { SetValue(LastMessageProperty, value); }
+        }
+    }
+}
