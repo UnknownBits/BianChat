77f6b67cd83103888c0aa4603241e8bc1ad32baa
WinExp <1450422389@qq.com>
2023-03-30 21:07:55 +0800

    * 修改客户端

---
+++ Client/Views/ChatPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Client.Controls;
+using Client.Module;
+using ModernWpf.Controls;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Client.Views
+{
+    /// <summary>
+    /// ChatPage.xaml 的交互逻辑
+    /// </summary>
+    public partial class ChatPage : System.Windows.Controls.Page
+    {
+        public ChatPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            MessageListBox.ItemsSource = Values.MessagesList;
+            if (Values.TcpSocket == null || !Values.TcpSocket.IsLogin) {
+                ContentDialog dialog = new ContentDialog
+                {
+                    Title = "提示",
+                    Content = "请到登录页进行登录。",
+                    PrimaryButtonText = "跳转至登录页",
+                    DefaultButton = ContentDialogButton.Primary
+                };
+                dialog.Closing += delegate { Values.MainWindow.RootNavigation.SelectedItem = Values.MainWindow.Account; };
+                DialogTools.ShowDialog(dialog);
+            }
+        }
+
+        /// <summary>
+        /// 发送按钮处理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            SendButton.IsEnabled = false;
+            MessageTextBox.IsEnabled = false;
+
+            Values.TcpSocket.SendPacket(TcpSocket.PacketType.Message_Messages, MessageTextBox.Text);
+
+            SendButton.IsEnabled = true;
+            MessageTextBox.IsEnabled = true;
+        }
+    }
+}
